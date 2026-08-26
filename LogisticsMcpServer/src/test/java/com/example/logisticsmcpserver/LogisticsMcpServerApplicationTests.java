67de14dd554f8438286d4106fd8665cf0240b05a
buithaison17 <125809724+buithaison17@users.noreply.github.com>
2026-08-26 21:23:10 +0700

    Upload LogisticsMcpServer/src/test/java/com/example/logisticsmcpserver/LogisticsMcpServerApplicationTests.java

---
+++ LogisticsMcpServer/src/test/java/com/example/logisticsmcpserver/LogisticsMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logisticsmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
